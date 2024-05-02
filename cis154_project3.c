9c77dd56c32070525432cc14f5fd40661552eee5
文及信 Văn Cấp Tín (Victor) <62981175+dimsumenjoyer@users.noreply.github.com>
2024-05-02 12:28:59 -0400

    Add files via upload

--- cis154_project3.c
+++ cis154_project3.c
@@ -26,6 +26,8 @@ void writeCipherText(char* fileName, char* text, int offSet, int cardinality);
 int encryptText(void);
 int decryptText(void);
 void displayMenu(void);
+void menuInput(int userInput);
+char* concatenateString(char* s1);
 
 int main(void)
 {
@@ -36,9 +38,9 @@ int main(void)
 void displayMenu(void)
 {
     bool done = false;
+    int userInput;
     while (!done)
     {
-        int userInput;
         puts("Welcome to the NECC File Encryption and Decryption Program!\nPlease select an option:\n1. Encrypt File\n2. Decrypt File\n3. Exit\nEnter your choice: ");
         scanf("%d", &userInput);
         switch(userInput)
@@ -48,21 +50,36 @@ void displayMenu(void)
             default: puts("Error: Invalid Input.\n");
         }
     }
+    menuInput(userInput);
+}
+
+void menuInput(int userInput)
+{
+    char* userInputedFileName;
+    switch(userInput)
+    {
+        case 1: puts("Please enter the name of the plaintext file you wish to encrypt (no extension): ");
+        scanf("%s", &userInputedFileName);
+        char* fileName = concatenateString(userInputedFileName); //fileName.necc
+        break;// encrypt file FILE *file = fopen("numberFile.md", "w");
+
+        case 2:
+        break;
+    }
 }
 
 void readPlainText(char* buffer, char* fileName)
 {
-
+    FILE *file = fopen(fileName, "r");
 }
 
 void readCipherText(char* fileName, int *offSet, int *cardinality)
 {
-
+    
 }
 
-void writeCipherText(char* fileName, char* text, int offSet, int ccardinality)
+void writeCipherText(char* fileName, char* text, int offSet, int cardinality)
 {
-    //int cardinalityOfStr = strlen(str);
 }
 
 int encryptText(void)
@@ -73,4 +90,24 @@ int encryptText(void)
 int decryptText(void)
 {
 
+}
+
+char* concatenateString(char* s1)
+{
+    char* s2 = ".necc";
+    int cardinality = strlen(s1) + strlen(s2);
+    char* fileName = (char*)malloc(cardinality);
+    int currentIndex = 0;
+    for (int i = 0; i < strlen(s1); i++)
+    {
+        fileName[currentIndex] = s1[i];
+        currentIndex++;
+    }
+    for (int i = 0; i < strlen(s2); i++)
+    {
+        fileName[currentIndex] = s2[i];
+        currentIndex++;
+    }
+    fileName[currentIndex] = '\0';
+    return fileName;
 }
